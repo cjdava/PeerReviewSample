495249337d59772afd1a0305eef8a233f7fcda46
cjdava <davachristianjoshua@gmail.com>
2026-03-02 17:54:33 +0800

    initial project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PeerReviewSample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
